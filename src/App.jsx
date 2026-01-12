2edabee8681d0ab386536673b47e9e5d25019b1e
elian811025 <aa0906496750@gmail.com>
2026-01-12 22:27:50 +0800

    Add Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Hero } from './components/Hero'
 import { AboutMe } from './components/AboutMe'
 import { EvolutionLab } from './components/EvolutionLab'
 import { Footer } from './components/Footer'
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
     return (
@@ -10,6 +12,9 @@ function App() {
             <AboutMe />
             <EvolutionLab />
             <Footer />
+            {/* Vercel Tracking (Invisible to users) */}
+            <Analytics />
+            <SpeedInsights />
         </div>
     )
 }
